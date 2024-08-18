7d9814183e2ef1e3ec0e6763ce09a6fd611c7773
Jesús Ssngonzalo Martínez <jesanma97@gmail.com>
2024-08-18 14:03:04 +0200

    Hexagonal Structure and spring project initialitation

---
+++ src/main/java/com/diverger/RestAPIStarWars/RestApiStarWarsApplication.java
@@ -0,0 +1,13 @@
+package com.diverger.RestAPIStarWars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiStarWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiStarWarsApplication.class, args);
+	}
+
+}
