9e33d4d37fdba315f17ce3cc38d6784279a4af67
Jesanma <64873639+jesanma97@users.noreply.github.com>
2024-08-18 14:03:56 +0200

    Hexagonal Structure and spring project initialitation (#1)

---
+++ src/test/java/com/diverger/RestAPIStarWars/RestApiStarWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diverger.RestAPIStarWars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiStarWarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
